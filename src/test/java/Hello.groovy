31881255c30751205d56736ea27212c7034c7726
yijun.wang <audestick@gmail.com>
2017-05-18 07:58:14 -0700

    更新

---
+++ src/test/java/Hello.groovy
@@ -0,0 +1,17 @@
+/**
+ * Created by lrkin on 2017/5/18.
+ */
+class Hello {
+    static main(def args) {
+        def str1 = "编程语言"
+        def str2 = "Groovy"
+        println "$str1:$str2"
+        println "$str2"
+
+        def str5 = "你,好"
+        def split = str5.split(",")
+        for (item in split) {
+            println item
+        }
+    }
+}
